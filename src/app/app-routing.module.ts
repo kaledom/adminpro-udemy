6c0a1d8869843ad60a45f5f4d419e15690fa2bf3
Luis Maria Anselmo Aguilar <lmanselmoa@gmail.com>
2020-06-29 12:07:24 +0200

    Modificaciones Módulos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,32 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './login/login.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
 import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
+import { PagesModule } from './pages/pages.module';
 
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PagesComponent,
-    children: [
-      { path: 'dashboard', component: DashboardComponent },
-      { path: 'progress', component: ProgressComponent },
-      { path: 'graficas1', component: Graficas1Component },
-      { path: '', redirectTo: '/dashboard', pathMatch: 'full' }
-    ]
-  },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
+  // { path: 'pages', loadChildren: () => import( `./pages/pages.module` ).then( m => m.PagesModule ) },
   { path: '**', component: NopagefoundComponent }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot( routes, { useHash: true } )],
+  imports: [
+    PagesModule,
+    RouterModule.forRoot( routes, { useHash: true } )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
